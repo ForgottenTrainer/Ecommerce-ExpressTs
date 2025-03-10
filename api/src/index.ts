b370f4e7a731fe0e34b833e62409d419e3a92b46
ForgottenTrainer <rogerthehedhog.98@gmail.com>
2025-03-10 17:30:14 -0500

    jsonparsing

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,11 @@
-import express, {Router} from 'express';
+import express, {json, urlencoded} from 'express';
 import productsRouter from './routes/products';
 
 const app = express()
 const port = 3000
+
+app.use(json())
+app.use(urlencoded({extended: false}))
 
 app.get('/', (req,res) => {
     res.send('Hola')
